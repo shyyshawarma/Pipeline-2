b0a0ecc020c96e2cdb0d039fa664dee897ccdf28
Abhinav Rajput <abhinavrajput179@gmail.com>
2025-07-26 14:57:26 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -8,6 +8,70 @@ from mmcv.cnn import build_norm_layer
 from timm.models.layers import DropPath
 
 
+def default_conv(in_channels, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(in_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=bias)
+
+def catcat(inputs1, inputs2):
+    return torch.cat((inputs1, inputs2), 2)
+class Cat(nn.Module):
+    def __init__(self):
+        super(Cat, self).__init__()
+
+    def forward(self, x1, x2):
+        return catcat(x1, x2)
+
+class Mix(nn.Module):
+    def __init__(self, m=-0.80):
+        super(Mix, self).__init__()
+        w = torch.nn.Parameter(torch.FloatTensor([m]), requires_grad=True)
+        w = torch.nn.Parameter(w, requires_grad=True)
+        self.w = w
+        self.mix_block = nn.Sigmoid()
+
+    def forward(self, fea1, fea2):
+        mix_factor = self.mix_block(self.w)
+        out = fea1 * mix_factor.expand_as(fea1) + fea2 * (1 - mix_factor.expand_as(fea2))
+        return out
+
+class Attention(nn.Module):
+    def __init__(self, dim):
+        super(Attention,self).__init__()
+        self.avg = nn.AdaptiveAvgPool2d((None, 1))
+        self.max = nn.AdaptiveMaxPool2d((1, None))
+        self.conv1x1 = default_conv(dim, dim//2, kernel_size=1, bias=True)
+        self.conv3x3 = default_conv(dim//2, dim, kernel_size=3, bias=True)
+        self.con3x3 = default_conv(dim, dim, kernel_size=3, bias=True)
+        self.GELU = nn.GELU()
+        self.mix1 = Mix(m=-1)
+        self.mix2 = Mix(m=-0.6)
+    def forward(self, x):
+        batch_size, channel, height, width = x.size()
+        x_h = self.avg(x)
+        x_w = self.max(x)
+        x_h = torch.squeeze(x_h, 3)
+        x_w = torch.squeeze(x_w, 2)
+        x_h1 = x_h.unsqueeze(3)
+        x_w1 = x_w.unsqueeze(2)
+        x_h_w = catcat(x_h, x_w)
+        x_h_w = x_h_w.unsqueeze(3)
+        x_h_w = self.conv1x1(x_h_w)
+        x_h_w = self.GELU(x_h_w)
+        x_h_w = torch.squeeze(x_h_w, 3)
+        x1, x2 = torch.split(x_h_w, [height, width], 2)
+        x1 = x1.unsqueeze(3)
+        x2 = x2.unsqueeze(2)
+        x1 = self.conv3x3(x1)
+        x2 = self.conv3x3(x2)
+        mix1 = self.mix1(x_h1, x1)
+        mix2 = self.mix2(x_w1, x2)
+        x1 = self.con3x3(mix1)
+        x2 = self.con3x3(mix2)
+        matrix = torch.matmul(x1, x2)
+        matrix = torch.sigmoid(matrix)
+        final = torch.mul(x, matrix)
+        final = x + final
+        return final
+
 def cat(x1, x2):
     diffY = x2.size()[2] - x1.size()[2]
     diffX = x2.size()[3] - x1.size()[3]
@@ -161,37 +225,62 @@ class BiasFree_LayerNorm(nn.Module):
         return x / torch.sqrt(sigma + 1e-5) * self.weight
 
 
-class WithBias_LayerNorm(nn.Module):
-    def __init__(self, normalized_shape):
-        super(WithBias_LayerNorm, self).__init__()
-        if isinstance(normalized_shape, numbers.Integral):
-            normalized_shape = (normalized_shape,)
-        normalized_shape = torch.Size(normalized_shape)
-
-        assert len(normalized_shape) == 1
-
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.normalized_shape = normalized_shape
-
-    def forward(self, x):
-        mu = x.mean(-1, keepdim=True)
-        sigma = x.var(-1, keepdim=True, unbiased=False)
-        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
-
-
 class LayerNorm(nn.Module):
-    def __init__(self, dim, LayerNorm_type):
+    def __init__(self, dim):
         super(LayerNorm, self).__init__()
-        if LayerNorm_type == 'BiasFree':
-            self.body = BiasFree_LayerNorm(dim)
-        else:
-            self.body = WithBias_LayerNorm(dim)
+        self.body = BiasFree_LayerNorm(dim)
 
     def forward(self, x):
         h, w = x.shape[-2:]
         return to_4d(self.body(to_3d(x)), h, w)
 
+
+class se_block(nn.Module):
+    def __init__(self, inplanes, reduction=16):
+        super(se_block, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(inplanes, inplanes//reduction, 1, 1, 0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inplanes // reduction, inplanes, 1, 1, 0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        input = x
+        x = self.se(x)
+        return input*x
+
+class se_block(nn.Module):
+    def __init__(self, inplanes, reduction=16):
+        super(se_block, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(inplanes, inplanes//reduction, 1, 1, 0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inplanes // reduction, inplanes, 1, 1, 0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        input = x
+        x = self.se(x)
+        return input*x
+
+class residual_block(nn.Module):
+    def __init__(self,in_channels, out_channels, kernel_size=3):
+        super(residual_block, self).__init__()
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=True),
+            nn.GELU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=True),
+            se_block(out_channels, reduction=16)
+        )
+
+    def forward(self, x):
+        input = x
+        x = self.residual(x)
+        return input + x
 
 class FeedForward(nn.Module):
     def __init__(self, dim, bias):
@@ -358,6 +447,55 @@ class BICA(nn.Module):
         return out
 
 
+# Frequency-Domain Pixel Attention
+class FDPA(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        self.conv = nn.Conv2d(dim, dim*2, 3, 1, 1)
+
+        self.conv1 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.conv2 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.alpha = nn.Parameter(torch.zeros(dim, 1, 1))
+        self.beta = nn.Parameter(torch.ones(dim, 1, 1))
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+
+        x2_fft = torch.fft.fft2(x2)
+        
+        # Get phase
+        phase = torch.angle(x2_fft)
+        
+        # Reconstruct complex tensor with unit magnitude and phase
+        phase_only_fft = torch.polar(torch.ones_like(phase), phase)
+        
+        # Apply inverse FFT to get phase-only attention
+        out = torch.fft.ifft2(phase_only_fft)
+        out = torch.real(out)  # Discard imaginary part (can also use torch.abs)
+
+        return out * self.alpha + x * self.beta
+
+
+class HybridDomainAttention(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.norm1 = LayerNorm(dim)
+        self.norm2 = LayerNorm(dim)
+        self.ffn = FeedForward(dim, bias=False)
+        self.fpa = FDPA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        out = self.fpa(x)
+        s_attn = self.conv(self.pool(self.norm1(out)))
+        out = s_attn * out
+        out = x + out
+        return out + self.ffn(self.norm2(out))
+
+
 
 
 class ECA(nn.Module):
@@ -408,28 +546,40 @@ class UIR_PolyKernel(nn.Module):
     def __init__(self, in_channels=3, out_channels=3, dim=36, bias=False):
         super().__init__()
         self.input_embed = nn.Conv2d(in_channels, dim, 1)
-        self.encoder_level1 = BICA(dim)
+        self.encoder_level1 = HybridDomainAttention(dim)
         self.down1_2 = Downsample(dim)
         self.encoder_level2 = LKABlock(dim * 2)
+        self.attention_lka1_to_lka2 = Attention(dim * 2)
         self.down2_3 = Downsample(dim * 2)
         self.encoder_level3 = LKABlock(dim * 4)
+        self.attention_lka2_to_csc = Attention(dim * 4)
         self.bottleneck = CSC_Block(dim * 4)
 
         self.eca_level3 = ECA(dim * 4)
         self.eca_level2 = ECA(dim * 2)
         self.eca_level1 = ECA(dim)
+        self.eca_atteention_lka1_to_lka2 = ECA(dim * 2)
+        self.eca_atteention_lka2_to_csc = ECA(dim * 4)
 
+        self.reduce_before_aha= nn.Conv2d(dim * 8, dim * 4, 1, bias=bias)
+        self.attention_csc_to_lka2 = Attention(dim * 4)
         self.reduce_chan_level3 = nn.Conv2d(dim * 8, dim * 4, 1, bias=bias)
         self.decoder_level3 = LKABlock(dim * 4)
         self.up3_2 = Upsample(dim * 4)
+        self.reduce_before_aha2 = nn.Conv2d(dim * 4, dim * 2, 1, bias=bias)
+        self.attention_lka2_to_lka1 = Attention(dim * 2)
         self.reduce_chan_level2 = nn.Conv2d(dim * 4, dim * 2, 1, bias=bias)
         self.decoder_level2 = LKABlock(dim * 2)
         self.up2_1 = Upsample(dim * 2)
         self.reduce_chan_level1 = nn.Conv2d(dim * 2, dim, 1, bias=bias)
-        self.decoder_level1 = BICA(dim)
+        self.decoder_level1 = HybridDomainAttention(dim)
 
         self.final_conv = nn.Conv2d(dim, out_channels, 1)
         self.norm = nn.Sigmoid()
+
+        self.start_conv = default_conv(in_channels=3, out_channels=256, kernel_size=3, bias=True)
+        self.Residual_block = residual_block(in_channels=256, out_channels=256, kernel_size=3)
+        self.final_conv_drb = default_conv(in_channels=256, out_channels=3, kernel_size=3, bias=True)
 
     def forward(self, x):
         inp = self.input_embed(x)
@@ -458,7 +608,17 @@ class UIR_PolyKernel(nn.Module):
         inp_dec_level1 = self.reduce_chan_level1(inp_dec_level1)
         out_dec_level1 = self.decoder_level1(inp_dec_level1)
 
-        return self.norm(self.final_conv(out_dec_level1) + x)
+
+        conv = self.start_conv(x)
+        Residual_block1 = self.Residual_block(conv)
+        Residual_block2 = self.Residual_block(Residual_block1)
+        Residual_block3 = self.Residual_block(Residual_block2)
+        Residual_block4 = self.Residual_block(Residual_block3)
+        Residual_block5 = self.Residual_block(Residual_block4)
+        Residual_block5 = conv + Residual_block5
+        result2 = self.final_conv_drb(Residual_block5)
+
+        return self.norm(self.final_conv(out_dec_level1) + result2)
 
 
 if __name__ == '__main__':
